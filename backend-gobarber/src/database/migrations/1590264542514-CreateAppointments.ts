96618f6fd6c8f97436ae3174bd744425c81c5205
Carlos Bordalo <carlosvitor6@hotmail.com>
2020-05-24 20:47:34 -0300

    feat: Connect to Database by TypeORM Repository

--- backend-gobarber/src/database/migrations/1590264542514-CreateAppointments.ts
+++ backend-gobarber/src/database/migrations/1590264542514-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1590264542514
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
